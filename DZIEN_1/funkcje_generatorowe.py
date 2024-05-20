bb27ce498b47d462541fd6069423f8909dfd10fa
albim72 <marcin.albiniak72@gmail.com>
2024-05-20 15:21:14 +0200

    Update funkcje_generatorowe.py

--- DZIEN_1/funkcje_generatorowe.py
+++ DZIEN_1/funkcje_generatorowe.py
@@ -15,3 +15,34 @@ print(list(liczby_gen()))
 
 for p in liczby_gen():
     print(p)
+
+#przykład 2
+
+def wznowienia(n,k):
+    print("wstrzymanie działania...")
+    yield 1001
+    print("wznowienie działania...")
+    yield n+k
+    print("działanie - dodawanie. Wstrzymanie")
+    print("wznowienie działania...")
+    n = 3*n
+    yield n - k
+    print("działanie - odejmowanie. Wstrzymanie")
+    print("wznowienie działania...")
+    yield n * k
+    print("działanie - mnożenie. Wstrzymanie")
+    print("wznowienie działania...")
+    yield n / k
+    print("działanie - dzielenie. Wstrzymanie")
+
+print(wznowienia(6,7))
+print(list(wznowienia(6,7)))
+
+print("_________________________________")
+
+for i in wznowienia(6,8):
+    if i == 1001:
+        continue
+    print(f"yield zwraca wartość: {i}")
+
+
