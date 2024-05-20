bd4b7b9f167290a077271a70be338e154b3ccaff
albim72 <marcin.albiniak72@gmail.com>
2024-05-20 13:18:43 +0200

    Update funkcje_no2.py

--- DZIEN_1/funkcje_no2.py
+++ DZIEN_1/funkcje_no2.py
@@ -3,3 +3,7 @@ import math
 #funkcja 1
 def gx(n,m=4,k=3,b=7):
     return math.sqrt(n+m)*k-b
+
+#funkcja 2
+def rank_list(*lang,nrrank):
+    print(f'ranking nr {nrrank} -> 1.{lang[0]}, 2.{lang[1]}, 3.{lang[2]}')
