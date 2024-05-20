b0402e955034c1b2074fb34bd7c8bb3f7764c966
albim72 <marcin.albiniak72@gmail.com>
2024-05-20 11:31:19 +0200

    Update wprowadzenie.py

--- DZIEN_1/wprowadzenie.py
+++ DZIEN_1/wprowadzenie.py
@@ -20,3 +20,50 @@ print(info)
 #CTRL+D - powielanie bloków/linii tesktów
 #CTRL +/ - komentowanie/odkomentowanie
 
+#kolekcja - lista
+
+imiona = ["Jan","Piotr","Anna","Nadia","Jan","Tomek","Maria","Anna","Jan"]
+print(imiona)
+print(imiona[0])
+print(imiona[2:4])
+print(imiona[1:])
+print(imiona[:5])
+print(imiona[-1])
+print(imiona[-2])
+
+imionaparzyste = imiona[::2]
+print(imionaparzyste)
+imiona.append("Karol")
+print(imiona)
+imiona.insert(3,"Leon")
+print(imiona)
+
+imiona.remove("Nadia")
+print(imiona)
+del imiona[5]
+print(imiona)
+
+imen = enumerate(imiona,111)
+for i,wartosc in imen:
+    print(f'index -> {i}, wartość: {wartosc}')
+
+noweimie = imiona
+qimie = list(imiona)
+pimie = imiona[:]
+
+
+print(imiona)
+print(noweimie)
+print(qimie)
+print(pimie)
+
+noweimie.append("Kunegunda")
+
+print(imiona)
+print(noweimie)
+print(qimie)
+print(pimie)
+
+
+
+
