969d292d6d4e4a131ce639a8cf67ae73b366208d
albim72 <marcin.albiniak72@gmail.com>
2024-05-20 13:45:32 +0200

    Update funkcja_w_funkcji.py

--- DZIEN_1/funkcja_w_funkcji.py
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -24,3 +24,26 @@ print(osoba(witaj,"Leon"))
 print(osoba(konkurs,"Leon","Kraków",56))
 
 print(multiosoba("Anna",19,88))
+
+#przykład 2
+
+def rejestracja(oplata):
+    def lista(nrstartowy):
+        return f"Twój numer startowy: {nrstartowy}"
+
+    def brak():
+        return "Brak wpłaty. Wpłać w ciągu dwóch tygodni!"
+
+    def error():
+        return "błąd wpłaty -> 0 - brak, 1 - wpłata"
+
+    if oplata == 1:
+        return lista
+    elif oplata == 0:
+        return brak
+    else:
+        return error
+
+print(rejestracja(1)(543))
+print(rejestracja(0)())
+print(rejestracja(-11)())
