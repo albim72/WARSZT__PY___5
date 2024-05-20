d731a84d48f3b88e8ada9e47a71e71683001dedb
albim72 <marcin.albiniak72@gmail.com>
2024-05-20 11:08:02 +0200

    Create wprowadzenie.py

---
+++ DZIEN_1/wprowadzenie.py
@@ -0,0 +1,22 @@
+print("pierwsza linia na dobre otwarcie...")
+#komentarz podstawowy
+"""
+komentarz dokumentacyjny
+wieloliniowy
+abc
+"""
+
+info = """
+to nie tylko komentarz
+ale wieloliniowy tekst,
+który moge wyświetlić
+wewnątrz funkcji print...
+"""
+
+print(info)
+print(info)
+print(info)
+
+#CTRL+D - powielanie bloków/linii tesktów
+#CTRL +/ - komentowanie/odkomentowanie
+
