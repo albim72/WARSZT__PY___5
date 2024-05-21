a1a501ad5351da25f874f239f5e793897ab9803c
albim72 <marcin.albiniak72@gmail.com>
2024-05-21 11:08:39 +0200

    Update main.py

--- DZIEN_2/OSOBY/main.py
+++ DZIEN_2/OSOBY/main.py
@@ -2,3 +2,6 @@ from osoba import Osoba
 
 os1 = Osoba("Jan",45,87,173)
 print(os1)
+nlat = int(input("podaj liczbę lat: "))
+print(f'wiek za {nlat} lat -> {os1.wiek_za_n_lat(nlat)}')
+print(f'czy osoba jest pracownikiem: {os1.czypracownik()}')
