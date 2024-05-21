f6941b0b4e545f03554d38d2c1a68015aea665d2
albim72 <marcin.albiniak72@gmail.com>
2024-05-21 11:14:29 +0200

    Update osoba.py

--- DZIEN_2/OSOBY/osoba.py
+++ DZIEN_2/OSOBY/osoba.py
@@ -6,23 +6,28 @@ class Osoba:
         self.wzrost = wzrost
         self.kolor_oczy = "brązowe"
         self.info()
-        
+
     def __repr__(self):
         return (f"Dane osoby ->\n"
                 f"imię: {self.imie}\n"
                 f"wiek: {self.wiek} lat\n"
                 f"waga: {self.waga} kg\n"
                 f"wzrost: {self.wzrost} cm")
-        
-        
+
+
     def info(self)->None:
         print("Tworzenie nowej instancji klasy Osoba -> obiekt Osoba")
-        
+
     def wiek_za_n_lat(self,n):
         return self.wiek + n
-    
+
     def czypracownik(self):
         return False
-    
-    
-        
+
+    def get_kolor_oczu(self):
+        return self.kolor_oczy
+
+    def set_kolor_oczu(self,nowykolor):
+        self.kolor_oczy = nowykolor
+
+
