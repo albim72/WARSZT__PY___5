1a1ef76276d02da2263d2aed0f665e27981fd3f4
albim72 <marcin.albiniak72@gmail.com>
2024-05-21 11:38:22 +0200

    Update main.py

--- DZIEN_2/OSOBY/main.py
+++ DZIEN_2/OSOBY/main.py
@@ -1,5 +1,7 @@
 from osoba import Osoba
+from pracownik import Pracownik
 
+print("_____________ klasa Osoba _____________")
 os1 = Osoba("Jan",45,87,173)
 print(os1)
 nlat = int(input("podaj liczbę lat: "))
@@ -8,3 +10,12 @@ print(f'czy osoba jest pracownikiem: {os1.czypracownik()}')
 
 os1.set_kolor_oczu("niebieski")
 print(f'nowy kolor: {os1.get_kolor_oczu()}')
+
+print("_____________ klasa Pracownik _____________")
+
+pr1 = Pracownik("Ala",30,56,167,"ABC","dyrektor",5,10900)
+print(pr1)
+nlat = int(input("podaj liczbę lat: "))
+print(f'wiek za {nlat} lat -> {pr1.wiek_za_n_lat(nlat)}')
+print(f'czy osoba jest pracownikiem: {pr1.czypracownik()}')
+pr1.print_pracownik()
