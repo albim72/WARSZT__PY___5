4a8bd4a1218d4be61b4aa37fad10987b808bea6c
albim72 <marcin.albiniak72@gmail.com>
2024-05-21 14:14:31 +0200

    Create xyz.py

---
+++ DZIEN_2/abstrakcja/xyz.py
@@ -0,0 +1,20 @@
+from prototyp import Prototyp
+
+class XYZ(Prototyp):
+    def __init__(self, x,a,b):
+        super().__init__(x)
+        self.a=a
+        self.b=b
+
+    def policz(self):
+        return self.x*(self.a+self.b)
+
+    def policz_x(self):
+        return self.a-self.x*self.b
+
+    def info(self, msg):
+        return msg*3
+
+
+
+
