b57ae9d1d7edf287c1a85d368c12fe07134dea13
albim72 <marcin.albiniak72@gmail.com>
2024-05-21 14:13:41 +0200

    Update prototyp.py

--- DZIEN_2/abstrakcja/prototyp.py
+++ DZIEN_2/abstrakcja/prototyp.py
@@ -3,15 +3,19 @@ from abc import ABC, abstractmethod
 class Prototyp(ABC):
     def __init__(self,x):
         self.x=x
-        
+
     def msg(self):
         print("metoda nieabstrakcyjna klasy abstrakcyjnej")
-        
+
     @abstractmethod
     def policz(self):
         pass
-    
+
     @abstractmethod
     def policz_x(self):
         return 5*self.x
-    
+
+    @abstractmethod
+    def info(self,msg):
+        pass
+
