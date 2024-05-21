408345605fa63125675f919afba34ed6608e1ba9
albim72 <marcin.albiniak72@gmail.com>
2024-05-21 15:01:36 +0200

    Update main.py

--- DZIEN_2/FIGURY/main.py
+++ DZIEN_2/FIGURY/main.py
@@ -1,6 +1,19 @@
 from trojkat import Trojkat
+from prostokat import Prostokat
 
 tr = Trojkat(5.6,6.8)
-print(f'pole figury {tr.__class__.__name__} wynosi: {tr.policz_pole()} cm2')
+print(f'pole figury {tr.__class__.__name__} wynosi: {tr.policz_pole():.2f} cm2')
+
+print("_"*50)
+
+pr1 = Prostokat(4.4,5.2)
+print(f'pole figury {pr1.__class__.__name__} wynosi: {pr1.policz_pole():.2f} cm2')
+print(type(pr1))
+
+print("_"*50)
+
+pr2 = Prostokat(5,5)
+print(f'pole figury {pr2.__class__.__name__} wynosi: {pr2.policz_pole():.2f} cm2')
+print(type(pr2))
 
 print("_"*50)
