bea91ab151e0dd8ea1cdc801c10eadd040ccb428
albim72 <marcin.albiniak72@gmail.com>
2024-05-21 11:14:08 +0200

    Update main.py

--- DZIEN_2/OSOBY/main.py
+++ DZIEN_2/OSOBY/main.py
@@ -5,3 +5,6 @@ print(os1)
 nlat = int(input("podaj liczbę lat: "))
 print(f'wiek za {nlat} lat -> {os1.wiek_za_n_lat(nlat)}')
 print(f'czy osoba jest pracownikiem: {os1.czypracownik()}')
+
+os1.set_kolor_oczu("niebieski")
+print(f'nowy kolor: {os1.get_kolor_oczu()}')
