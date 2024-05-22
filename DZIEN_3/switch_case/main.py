0d612b66c63e372c9c9c95ab42e2631c8c46a7b8
albim72 <marcin.albiniak72@gmail.com>
2024-05-22 13:53:24 +0200

    Create main.py

---
+++ DZIEN_3/switch_case/main.py
@@ -0,0 +1,9 @@
+from operacje import calculate
+
+print("____ użycie instrunkcji match-case ________")
+calculate('+',5,6)
+calculate('-',7,6)
+calculate('*',2,11)
+calculate('/',5,3)
+calculate('/',5,0)
+calculate('?',52,6)
