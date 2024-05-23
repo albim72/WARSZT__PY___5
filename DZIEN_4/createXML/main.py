b8a970b524156e822b9230f36bfd3520c73528cc
albim72 <marcin.albiniak72@gmail.com>
2024-05-23 11:13:44 +0200

    Update main.py

--- DZIEN_4/createXML/main.py
+++ DZIEN_4/createXML/main.py
@@ -32,3 +32,40 @@ kolor.text = 'czarna perła'
 
 klima = SubElement(wyp_dod,'klimatyzacja')
 klima.text = "R75345345"
+
+#drugi samochód
+sam2 = SubElement(top,'samochod')
+
+id = SubElement(sam2,'id')
+id.text = 'sam2'
+
+marka = SubElement(sam2,'marka')
+marka.text = 'Subaru'
+
+model = SubElement(sam2,'model')
+model.text = 'Outback'
+
+poj = SubElement(sam2,'pojemnosc')
+poj.text = '2.4'
+
+rok = SubElement(sam2,'rocznik')
+rok.text = '2019'
+
+cena = SubElement(sam2,'cena')
+cena.text = '131000'
+
+wyp_dod = SubElement(sam2,'wyposazenie_dod')
+
+kolor = SubElement(wyp_dod,'kolor')
+kolor.text = 'czerwony metallic'
+
+klima = SubElement(wyp_dod,'klimatyzacja')
+klima.text = "FRT5667665"
+
+poddod = SubElement(wyp_dod,'dodtakowe_pod')
+poddod.text = "4"
+
+print(pretty(top))
+
+with open("subaru.xml","a",encoding="utf-8") as f:
+    f.write(pretty(top))
